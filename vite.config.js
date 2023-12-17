4a78976473c9ebecf6b403847f43d24ba6033868
RedRoll <redrollofficial@gmail.com>
2023-12-17 14:53:47 +0100

    test push

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/autonomous-project/"
+  base: "/autonomous-project/",
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  }
 });
